95d2a99ece96192c8e00cd4d302a1876cfadc02b
EVERTON OLIVEIRA <evertonbaixista@gmail.com>
2022-03-25 19:04:35 -0300

    finalizando auth tarefa amarelo

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -1,17 +1,41 @@
 package br.com.caelum.eats.pagamento;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableResourceServer
 public class EatsPagamentoServiceApplication {
 
+	public RequestInterceptor getInterceptorDeAutenticacao() {
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate template) {
+				org.springframework.security.core.Authentication  authentication = SecurityContextHolder.getContext().getAuthentication();
+				 if(authentication==null) {
+					 return;
+				 }
+				
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)authentication.getDetails();
+				template.header("Authorization", "Bearer" + details.getTokenValue());
+						
+			}
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EatsPagamentoServiceApplication.class, args);
 	}
---
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ResourceServerConfigurer.java
@@ -0,0 +1,20 @@
+package br.com.caelum.eats.pagamento;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
+
+	public void configure(HttpSecurity http) throws Exception {
+		
+
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.PUT, "/pagamentos/{id}")
+			.hasAnyRole("CLIENTE","ADMINISTRATIVO");
+		
+
+	}
+}
